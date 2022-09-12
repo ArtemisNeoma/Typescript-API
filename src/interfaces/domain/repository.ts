0ff5674ac3b00a8314652cd8feb5d8a227fa4af1
Neo-WRN <nicolas.destito@warren.com.br>
2022-09-12 18:34:03 -0300

    feat: created ReadAllType and added it to IEndPointsRepository

--- src/interfaces/domain/repository.ts
+++ src/interfaces/domain/repository.ts
@@ -1,7 +1,6 @@
 import {
   DeleteResult,
   Document,
-  FindCursor,
   InsertOneResult,
   UpdateResult,
   WithId,
@@ -9,11 +8,11 @@ import {
 import { ICustomer } from './customer/repository';
 
 export type IVariableDatabase = Map<number, ICustomer>;
-
+export type ReadAllType = WithId<Document>[];
 export interface IEndPointsRepository {
   create(entity: object): Promise<InsertOneResult<Document>>;
   read(id: number): Promise<WithId<Document> | null>;
   update(id: number, newEntity: object): Promise<UpdateResult>;
-  readAll(): Promise<WithId<Document>[]>;
+  readAll(): Promise<ReadAllType>;
   delete(id: number): Promise<DeleteResult>;
 }
