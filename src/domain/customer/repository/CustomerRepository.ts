09a2f599c9e9a18ebb120b961e42983d45f7336c
Neo-WRN <nicolas.destito@warren.com.br>
2022-09-13 18:51:18 -0300

    refactor: created collection property at CustomerRepository

--- src/domain/customer/repository/CustomerRepository.ts
+++ src/domain/customer/repository/CustomerRepository.ts
@@ -4,48 +4,36 @@ import {
   ICustomer,
 } from '@interfaces/domain/customer/repository';
 import { IDatabaseClient } from '@interfaces/infrastructure';
+import { Collection, Document } from 'mongodb';
 import { inject, injectable } from 'tsyringe';
 
 @injectable()
 export default class CustomerRepository implements IRepositoryCustomer {
+  private collection: Collection<Document>;
   constructor(
     @inject(tokens.DatabaseClient)
     private dbClient: IDatabaseClient,
-  ) {}
+  ) {
+    this.collection = dbClient.getInstance().collection('Customer');
+  }
 
   public async create(entity: ICustomer) {
-    return await this.dbClient
-      .getInstance()
-      .collection('Customer')
-      .insertOne(entity);
+    return await this.collection.insertOne(entity);
   }
 
   public async read(id: number) {
-    return await this.dbClient
-      .getInstance()
-      .collection('Customer')
-      .findOne({ _id: id });
+    return await this.collection.findOne({ _id: id });
   }
 
   public async readAll() {
-    return await this.dbClient
-      .getInstance()
-      .collection('Customer')
-      .find()
-      .toArray();
+    return await this.collection.find().toArray();
   }
 
   public async update(id: number, newEntity: ICustomer) {
-    return await this.dbClient
-      .getInstance()
-      .collection('Customer')
-      .updateOne({ _id: id }, newEntity);
+    return await this.collection.updateOne({ _id: id }, newEntity);
   }
 
   public async delete(id: number) {
-    return await this.dbClient
-      .getInstance()
-      .collection('Customer')
-      .deleteOne({ _id: id });
+    return await this.collection.deleteOne({ _id: id });
   }
 }
