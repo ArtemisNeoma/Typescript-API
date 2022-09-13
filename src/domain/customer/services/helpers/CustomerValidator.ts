065e907aefdbf41240d2b04394e8e49e65ef4a8a
Neo-WRN <nicolas.destito@warren.com.br>
2022-09-13 18:36:56 -0300

    refactor: removed checkUnique and updated typing on CustomerValidator

--- src/domain/customer/services/helpers/CustomerValidator.ts
+++ src/domain/customer/services/helpers/CustomerValidator.ts
@@ -1,4 +1,3 @@
-import { IVariableDatabase } from '@interfaces/domain/repository';
 import { ICustomerValidator } from '@interfaces/domain/customer/services/validation';
 import { ICustomer } from '@interfaces/domain/customer/repository';
 import { inject, injectable } from 'tsyringe';
@@ -8,35 +7,19 @@ import { tokens } from '@di/tokens';
 export default class CustomerValidator implements ICustomerValidator {
   readonly _getCep: (value: string) => Promise<boolean>;
   readonly _isCpfValid: (cpf: string) => boolean;
-  readonly _checkUnique: (
-    value: string,
-    fieldName: keyof ICustomer,
-    database: IVariableDatabase,
-  ) => boolean;
   constructor(
     @inject(tokens.getCep)
     getCep: (value: string) => Promise<boolean>,
     @inject(tokens.isCpfValid)
     isCpfValid: (cpf: string) => boolean,
-    @inject(tokens.checkUnique)
-    checkUnique: (
-      value: string,
-      fieldName: keyof ICustomer,
-      database: IVariableDatabase,
-    ) => boolean,
   ) {
     this._getCep = getCep;
     this._isCpfValid = isCpfValid;
-    this._checkUnique = checkUnique;
   }
 
-  public async validate(user: ICustomer, database: IVariableDatabase) {
+  public async validate(user: ICustomer) {
     if (!(await this._getCep(user.postal_code)))
       throw Error(`Postal Code ${user.postal_code} is invalid`);
     if (!this._isCpfValid(user.cpf)) throw Error(`CPF ${user.cpf} is invalid`);
-    if (!this._checkUnique(user.email, 'email', database))
-      throw Error(`Email ${user.email} already exists`);
-    if (!this._checkUnique(user.cpf, 'cpf', database))
-      throw Error(`CPF ${user.cpf} already exists`);
   }
 }
