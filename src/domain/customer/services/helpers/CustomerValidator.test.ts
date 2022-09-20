3043a9713e5edf29c0f4359ddb47377eae85ae84
Neo-WRN <nicolas.destito@warren.com.br>
2022-09-20 14:10:18 -0300

    test: removed checkUnique references from CustomerValidator.test

--- src/domain/customer/services/helpers/CustomerValidator.test.ts
+++ src/domain/customer/services/helpers/CustomerValidator.test.ts
@@ -4,7 +4,6 @@ import CustomerValidator from './CustomerValidator';
 
 const userValidator = container.resolve(CustomerValidator);
 const mockMethods = {
-  checkUnique: jest.spyOn(userValidator, '_checkUnique').mockReturnValue(true),
   getCep: jest.spyOn(userValidator, '_getCep').mockResolvedValue(true),
   isCpfValid: jest.spyOn(userValidator, '_isCpfValid').mockReturnValue(true),
 };
@@ -25,19 +24,6 @@ describe('CustomerValidator', () => {
     });
     it('Should reject when isCpfValid returns false', async () => {
       mockMethods.isCpfValid.mockReturnValueOnce(false);
-      await expect(
-        userValidator.validate({} as ICustomer, mockDatabase),
-      ).rejects.toThrow();
-    });
-    it('Should reject when the first checkUnique returns false', async () => {
-      mockMethods.checkUnique.mockReturnValueOnce(false);
-      await expect(
-        userValidator.validate({} as ICustomer, mockDatabase),
-      ).rejects.toThrow();
-    });
-    it('Should reject when the second checkUnique returns false', async () => {
-      mockMethods.checkUnique.mockReturnValueOnce(true);
-      mockMethods.checkUnique.mockReturnValueOnce(false);
       await expect(
         userValidator.validate({} as ICustomer, mockDatabase),
       ).rejects.toThrow();
