9722f23ea59163b4f36fb43df5adf06d15e739ad
Neo-WRN <nicolas.destito@warren.com.br>
2022-09-20 14:11:10 -0300

    test: removed mockDatabase from CustomerValidator.test

--- src/domain/customer/services/helpers/CustomerValidator.test.ts
+++ src/domain/customer/services/helpers/CustomerValidator.test.ts
@@ -7,26 +7,21 @@ const mockMethods = {
   getCep: jest.spyOn(userValidator, '_getCep').mockResolvedValue(true),
   isCpfValid: jest.spyOn(userValidator, '_isCpfValid').mockReturnValue(true),
 };
-const mockDatabase = new Map<number, ICustomer>();
 
 describe('CustomerValidator', () => {
   describe('validate', () => {
     it('Should not throw error when all validations return true', async () => {
       await expect(
-        userValidator.validate({} as ICustomer, mockDatabase),
+        userValidator.validate({} as ICustomer),
       ).resolves.not.toThrow();
     });
     it('Should reject when getCep returns false', async () => {
       mockMethods.getCep.mockResolvedValueOnce(false);
-      await expect(
-        userValidator.validate({} as ICustomer, mockDatabase),
-      ).rejects.toThrow();
+      await expect(userValidator.validate({} as ICustomer)).rejects.toThrow();
     });
     it('Should reject when isCpfValid returns false', async () => {
       mockMethods.isCpfValid.mockReturnValueOnce(false);
-      await expect(
-        userValidator.validate({} as ICustomer, mockDatabase),
-      ).rejects.toThrow();
+      await expect(userValidator.validate({} as ICustomer)).rejects.toThrow();
     });
   });
 });
