4aead96565703f62ef109a0e4d694d3cad2deac1
Neo-WRN <nicolas.destito@warren.com.br>
2022-10-07 14:17:41 -0300

    refactor: changed createMiddleware to be created from a fabric function

--- src/middleware/customer/createMiddlewares/createMiddleware.ts
+++ src/middleware/customer/createMiddlewares/createMiddleware.ts
@@ -1,18 +1,16 @@
 import { RouteMiddleware } from '@interfaces/middleware';
 import StatusError from '@util/error';
 import { NextFunction, Request, Response } from 'express';
-import userSchema from './schema';
+import { AnySchema } from 'joi';
 
-export const createMiddleware: RouteMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const validateBody = await userSchema.validateAsync(req.body);
-    req.body = validateBody;
-    next();
-  } catch (error) {
-    next(new StatusError(422, `${error}`));
-  }
-};
+export const createMiddleware: RouteMiddleware =
+  (schema: AnySchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const validateBody = await schema.validateAsync(req.body);
+      req.body = validateBody;
+      next();
+    } catch (error) {
+      next(new StatusError(422, `${error}`));
+    }
+  };
